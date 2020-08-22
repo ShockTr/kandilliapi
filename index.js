b20a263363602396c785047d8544dce1b93ef7fb
ShockTr <borakayik@icloud.com>
2020-08-22 18:16:08 +0300

    Fixed some issues

--- index.js
+++ index.js
@@ -11,29 +11,28 @@ async function fetchDeprem(){
     await page.goto("http://www.koeri.boun.edu.tr/scripts/lst0.asp");
 
     const src = await page.evaluate(() => {
-        let srctext = document.getElementsByTagName("pre").item(0).innerText
-        return srctext;
+        return document.getElementsByTagName("pre").item(0).innerText;
     });
     let text = src.replace(delete1, '');
     text = text.split('\n');
     let depremler = [];
     for(let i = 0; 498 > i; i++){
         depremler[i] = {
-            "Tarih" : text[i].slice(0, 10),
-            "Saat" : text[i].slice(11, 19),
-            "Enlem" : text[i].slice(21, 28),
-            "Boylam" : text[i].slice(31, 38),
-            "Derinlik" : text[i].slice(46, 50),
-            "MD" : text[i].slice(55, 59),
-            "ML" : text[i].slice(60, 64),
-            "MW" : text[i].slice(65, 69),
-            "Yer" : text[i].slice(71, 121),
+            "Tarih" : text[i].slice(0, 10).trim(),
+            "Saat" : text[i].slice(11, 19).trim(),
+            "Enlem" : text[i].slice(21, 28).trim(),
+            "Boylam" : text[i].slice(31, 38).trim(),
+            "Derinlik" : text[i].slice(46, 50).trim(),
+            "MD" : text[i].slice(55, 59).trim(),
+            "ML" : text[i].slice(60, 64).trim(),
+            "MW" : text[i].slice(65, 69).trim(),
+            "Yer" : text[i].slice(71, 121).trim(),
             "Niteligi" : text[i].slice(121, ),
         }
     }
     browser.close()
     return depremler
-};
+}
 app.get('/', (req, res) => {
     fetchDeprem().then(data => {
         res.json({
